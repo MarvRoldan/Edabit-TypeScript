3ab6dc0600bfdcd9b624713e4534abbe0d046f0d
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-02 11:34:39 -0600

    Added solution easy-1

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+function sayHelloBye(name, num) {
+    var n = name[0].toUpperCase() + name.slice(1);
+    if (num === 1) {
+        console.log("Hello " + n);
+    }
+    else {
+        console.log("Bye " + n);
+    }
+}
+sayHelloBye("John", 0);
+function factorial(n) {
+    if (n < 0) {
+        return -1;
+    }
+    else if (n == 0) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+console.log(factorial(5));
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,4 +1,23 @@
 // -- Which Function Returns the Larger Number? --
-declare function whichIsLarger(f: number, g: number): "f" | "g" | "neither";
+function sayHelloBye(name: string, num: number) {
+  const n = name[0].toUpperCase() + name.slice(1);
 
+  if (num === 1) {
+    console.log("Hello " + n);
+  } else {
+    console.log("Bye " + n);
+  }
+}
+sayHelloBye("John", 0);
 
+// -- Recursion: Factorials --
+function factorial(n: number): number {
+  if (n < 0) {
+    return -1;
+  } else if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(5));
