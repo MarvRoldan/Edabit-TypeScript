653276c3a6c33b4c9798d9a79653d2041b6c3df6
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-01 23:42:28 -0600

    Added solution easy-2

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function triArea(base, height) {
     return (base * height) / 2;
 }
+console.log("Area of the triangle is: " + triArea(10, 5));
+function addition(a, b) {
+    return a + b;
+}
+console.log("The sum is: " + addition(10, 10));
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 // -- Area of a Triangle --
-function triArea (base: number, height: number): number {
-	return (base * height) / 2;
+function triArea(base: number, height: number): number {
+  return (base * height) / 2;
 }
+console.log("Area of the triangle is: " + triArea(10, 5));
+
+// -- Return the Sum of Two Numbers --
+function addition(a: number, b: number): number {
+  return a + b;
+}
+console.log("The sum is: " + addition(10, 10));
